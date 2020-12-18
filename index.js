4f791ed12c7d6e564c46891b80e01b280221f313
ZukaGap <zurabigaprindashvili@gmail.com>
2020-12-18 11:45:14 +0400

    file changes

--- index.js
+++ index.js
@@ -1,66 +1,15 @@
 const express = require("express");
+require("./db/mongoose");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
+
 const app = express();
-const swaggerJsDoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
+const port = process.env.PORT;
 
-const swaggerOptions = {
-  swaggerDefinition: {
-    info: {
-      version: "1.0.0",
-      title: "Customer API",
-      description: "Customer API Information",
-      contact: {
-        name: "Amazing Developer",
-      },
-      servers: ["http://localhost:5000"],
-    },
-  },
-  apis: ["index.js"],
-};
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
 
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
-// Routes
-/**
- * @swagger
- * /:
- *  get:
- *    description: Use to request all customers
- *    responses:
- *      '200':
- *        description: A successful response
- */
-app.get("", (req, res) => {
-  res.send("წერილი მოგივიდა დებილო !!!");
-});
-
-/**
- * @swagger
- * /help:
- *    get:
- *      description: Use to return all customers
- *      responses:
- *         '200':
- *           description: A successful response
- */
-app.get("/help", (req, res) => {
-  res.send("დაეხმარე ყველას !!!");
-});
-
-/**
- * @swagger
- * /*:
- *    get:
- *      description: Not found
- *      responses:
- *         '404':
- *           description: Not found
- */
-app.get("*", (req, res) => {
-  res.send("404");
-});
-
-app.listen(3000, () => {
-  console.log("Server started on 3000 port");
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
 });
